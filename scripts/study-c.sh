a070f0b65cac940b10b91d74ec11ec32c531f9db
marcosgabrielsr <marcosdevse@gmail.com>
2025-03-23 21:38:55 -0400

    doc: Update commentaries

--- scripts/study-c.sh
+++ scripts/study-c.sh
@@ -1,2 +1,2 @@
-# Command to organizate my study environment
+# Command to organizate my study environment to study C
 alias study_c="open_file ~/Documents/Books/'Modern C-Manning Publications - Jens Gustedt.pdf' && zed ~/Documents/Studies/C/book-codes"
